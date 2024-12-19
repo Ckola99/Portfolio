e9c32f26d1ce2a55b63c987e46692b2fc6b42c5f
Ckola99 <Christopherkola@gmail.com>
2024-12-19 10:12:20 +0200

    started portfolio

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Home from "./pages/Home"
+import Layout from "./components/Layout"
+function App() {
+ return (
+  <Router>
+    <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </Router>
+ )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'fira-code': ['Fira Code'],
+      },
+      colors: {
+        'midnight': '#01080E',
+        'navy-blue-primary': '#011627',
+        'navy-blue': '#011221',
+        'light-gray': '#607B96',
+        'mint': '#3C9D93',
+        'blue': '#4D5BCE',
+        'white': '#FFFFFF',
+        'orange-accent': '#FEA55F',
+        'mint-accent': '#43D9AD',
+        'peach-accent': '#E99287',
+        'violet-accent': '#C98BDF',
+        'line-color': '#1E2D3D'
+      }
+    },
+  },
+  plugins: [],
+}
