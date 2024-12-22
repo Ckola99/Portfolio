459f31eeba7173831c136455f087ac45dd96cf07
Christopher kola <Christopherkola@gmail.com>
2024-12-22 14:46:51 +0200

    backend

--- Backend/app.js
+++ Backend/app.js
@@ -5,7 +5,13 @@ const middleware = require('./utils/middleware')
 const githubRouter = require('./controllers/github')
 
 
-app.use(cors())
+app.use(cors(
+	{
+		origin: ["https://ckolaportfolio.vercel.app/"],
+		methods: ["POST", "GET"],
+		credentials: true
+	}
+))
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
